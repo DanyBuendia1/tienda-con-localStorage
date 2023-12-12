3787da76ff0119da61afa9dfe994291386f4ba7c
danyBuendia <danybuendia98@gmail.com>
2023-12-12 13:15:07 -0600

    ajustes responsive

--- home/compras/compras.js
+++ home/compras/compras.js
@@ -1,5 +1,8 @@
+let Productos = JSON.parse(sessionStorage.getItem("Productos")) || [];
+let sesion = JSON.parse(sessionStorage.getItem("Sesion"));
+
 document.querySelector(".texto").addEventListener("click",()=>{
-    let confirmar = confirm("¿Te fustaria agrgar productos?");
+    let confirmar = confirm("¿Te gustaria agregar productos?");
     if(confirmar)
     {
         window.location="../home.html";
@@ -10,8 +13,14 @@ let Sesion = JSON.parse(sessionStorage.getItem("Sesion"));
 //-----------Datos de Usuario-----------
 
 document.getElementById("name").innerHTML=`Bienvenid@ ${Sesion.name}`
-
-let Productos = JSON.parse(sessionStorage.getItem("Productos")) || [];
+function cerrarsesion(){
+  let confirmar = confirm(`${sesion.name}, ¿Te gustaria Cerrar Sesion?`);
+  if(confirmar)
+  {
+    window.location="../../index.html"
+    sessionStorage.clear();
+  }
+}
 
 if(Productos != "")
 {
@@ -25,27 +34,19 @@ if(Productos != "")
         contenedor.innerHTML+=`
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
         <div class="card" style="width: 18rem;">
+          <div class="card-body">
           <img src="../../img/${Productos[i].image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${Productos[i].name}</h5>
-            <p class="card-text">${Productos[i].description}</p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">precio: $${Productos[i].price}</li>
-          </ul>
-          <div class="card-body">
-            <a href="#" class="card-link btn btn-danger">restar</a>
-            <input type="number" value="0">
-            <a href="#" class="card-link btn btn-success" onclick="suma${Productos[i].id}()">sumar</a>
+            <h4 class="card-subtitle mb-2 text-muted">${Productos[i].name}</h4>
+            <p class="card-text">${Productos[i].description}. <br>
+            Precio: $${Productos[i].price}</p>
+            <a class="card-link btn btn-danger" onclick="resta${Productos[i].id}()">restar</a>
+            <input type="number" id="producto${Productos[i].id}" value="0">
+            <a class="card-link btn btn-success" onclick="suma${Productos[i].id}()">sumar</a>
+            <br><br>
+            <p>Total: </p>
           </div>
         </div>
       </div>
-      <script>
-      let ${Productos[i].id} = 0;
-      function suma${Productos[i].id}(){
-        ${Productos[i].id}++
-      }
-      </script>
-      `
+        `
     }
 }
